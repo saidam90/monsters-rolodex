203c37633d9a41fe7223855a0730e8361fd311a6
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-05-05 21:07:27 +0400

    Improve css

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,29 @@
-import { Component } from "react";
+// import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.style.css";
 
-class CardList extends Component {
-  render() {
-    // console.log(this.props.monsters);
-    // console.log("render from Cardlist");
-    const { monsters } = this.props;
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </div>
-    );
-  }
-}
+// class CardList extends Component {
+//   render() {
+//     // console.log(this.props.monsters);
+//     // console.log("render from Cardlist");
+//     const { monsters } = this.props;
+
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           return <Card monster={monster} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
